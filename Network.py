e20caab27046c1e0efcc65025fe5eaf0f16fb4e1
Davis-Grant <70356902+Davis-Grant@users.noreply.github.com>
2023-05-06 15:36:02 +0000

    simulation of ddos attack detection and response.

--- Network.py
+++ Network.py
@@ -1,202 +1,76 @@
+import sys
 import networkx as nx
 import random as rd
 
-class Packet:
-    def __init__(self, routerID, edgeID = [-1,-1], edgeD = 0): # for the edge ID, left is start and right is end respective to victim router
+#output to networkdata.txt
+sys.stdout = open('networkdata.txt', 'w')
 
+class Packet:
+    def __init__(self, routerID, edgeID = [-1,-1], edgeD = 0):
         self.routerID = routerID
         self.edgeID = edgeID
         self.edgeD = edgeD
 
-class Tree:
-    def __init__(self, edge = [], distance = 0):
-
-        self.recPath = []
-        self.combinedList = []
-        self.victim = G.Node
-
-    def combineLists(self, listOfEdge, listOfDistance):
-        for i in range(0, len(listOfEdge)):
-            self.combinedList[i] = [listOfEdge[i], listOfDistance[i]]
-            
-
-    
-
-
+# Create the network graph
 G = nx.Graph()
-recPath = nx.Graph()
 edgeList = [[1,2], [2,3], [3,4], [4,5], [5,6], [6,19], [18,19],
             [7,8], [8,9], [9,10], [10,11], [11,12], [12,19], 
             [13,14], [14,15], [15,16], [16,17], [17,18], [19,20]]
 
-for i in range (1,21):
-    G.add_node(i, prob = .8, id = i, packet = Packet(i, [i,i], 1), sendChance = .0008, distance = 0)
+for i in range(1, 21):
+    G.add_node(i, prob=.8, id=i, packet=Packet(i, [i, i], 1), sendChance=.0008, distance=0, rate_limit=10, sent_packets=0, user_type="normal", activity_level=1)
 
-emptyList = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+# Set the attributes for the DDoS attack nodes
+num_attackers = rd.randint(2, 5)
+ddos_nodes = rd.sample(range(1, 21), num_attackers)
+for node in ddos_nodes:
+    G.nodes[node]['sendChance'] = 0.8
+    G.nodes[node]['rate_limit'] = 20
+    G.nodes[node]['user_type'] = "attacker"
+    G.nodes[node]['activity_level'] = 10
+
+# Function to detect congestion in the network
+def detect_congestion(G, threshold_factor=5):
+    avg_edgeD = sum([G.nodes[node]['packet'].edgeD for node in G.nodes]) / len(G.nodes)
+    return avg_edgeD > threshold_factor * G.nodes[1]['packet'].edgeD
+
+# Function to prevent the DDoS attack
+def prevent_ddos(G, ddos_nodes):
+    for node in ddos_nodes:
+        G.nodes[node]['rate_limit'] = 1
+        G.nodes[node]['activity_level'] = 1
+
+# Simulate network traffic
 totalPacketsSent = 0
+attack_detected = False
+for _ in range(1000):
+    # Send packets for each node based on their sendChance and rate_limit
+    for node in G.nodes:
+        if rd.random() < G.nodes[node]['sendChance'] and G.nodes[node]['sent_packets'] < G.nodes[node]['rate_limit']:
+            totalPacketsSent += 1
+            G.nodes[node]['sent_packets'] += 1
+            # Simulate packet sending logic here
 
-attrs = {20: {"markCount": emptyList, "edgeList": [], "edgeDList": []}}
+    # Detect congestion and prevent DDoS attack if detected
+    if not attack_detected and detect_congestion(G):
+        print("Congestion detected! Preventing DDoS attack.")
+        prevent_ddos(G, ddos_nodes)
+        attack_detected = True
+        # Log details of the detected attack
+        print("Attack details:")
+        for node in ddos_nodes:
+            print(" - Attacker:", node, "Activity level:", G.nodes[node]['activity_level'])
 
-nx.set_node_attributes(G, attrs)
-G.add_edges_from(edgeList)
-attacker = rd.randint(1,19)
-G.nodes[attacker]['sendChance'] = .8
-attacker2 = rd.randint(1,19)
-G.nodes[attacker2]['sendChance'] = .8
+    # Reset sent_packets counter for the next simulation step
+    for node in G.nodes:
+        G.nodes[node]['sent_packets'] = 0
 
-def countPacket(nodeID):
-    startID = nodeID
-    G.nodes[20]['markCount'][startID] += 1
+print("Total packets sent:", totalPacketsSent)
 
-def countEdgePacket(node):
-    G.nodes[20]['edgeList'].append(node['packet'].edgeID)
-    G.nodes[20]['edgeDList'].append(node['packet'].edgeD)
+# Print the final state of the network after the simulation
+print("Final state of the network:")
+for node in G.nodes:
+    print("Node", node, "user_type:", G.nodes[node]['user_type'], "activity_level:", G.nodes[node]['activity_level'])
 
-def sendPacket(node, graph):
-
-    sendTo = 0
-    nl = [n for n in graph.neighbors(node['id'])]
-    length = len(nl)
-    for i in range(length):
-        if sendTo < nl[i]:
-            sendTo = nl[i]
-    
-    if node['id'] != 20:
-        #print("Hop To: ", sendTo)
-        G.nodes[sendTo]['packet'] = node['packet']
-
-        if rd.random() < G.nodes[sendTo]['prob']:
-            G.nodes[sendTo]['packet'] = Packet(G.nodes[sendTo]['id'])
-        node['packet'] = Packet(node['id'])
-        sendPacket(G.nodes[sendTo], G)
-
-    else:
-        #print(node['packet'].routerID)
-        countPacket(node['packet'].routerID)
-        graph.nodes[20]['packet'] = Packet(20)
-
-def sendEdgePacket(node, graph):
-    sendTo = 1
-    nl = [n for n in graph.neighbors(node['id'])]
-    length = len(nl)
-    for i in range(length):
-        if sendTo < nl[i]:
-            sendTo = nl[i]
-    
-    if node['id'] != 20:
-        #print("Hop To: ", sendTo)
-        graph.nodes[sendTo]['packet'] = node['packet']
-
-        if graph.nodes[sendTo]['prob'] > rd.random():
-            graph.nodes[sendTo]['packet'].edgeID[0] = graph.nodes[sendTo]['id']
-            graph.nodes[sendTo]['packet'].edgeID[1] = 0
-            graph.nodes[sendTo]['packet'].edgeD = 0
-
-            node['packet'] = Packet(node['id'], [node['id'], node['id']], 1)
-
-        else:
-            if graph.nodes[sendTo]['packet'].edgeD == 0:
-                graph.nodes[sendTo]['packet'].edgeID[1] = graph.nodes[sendTo]['id']
-            graph.nodes[sendTo]['packet'].edgeD += 1
-            node['packet'] = Packet(node['id'], [node['id'], node['id']], 0)
-
-        sendEdgePacket(G.nodes[sendTo], graph)
-
-    else:
-            countEdgePacket(node)
-            graph.nodes[20]['packet'] = Packet(20, [20, 20], 0)
-
-
-def nodeSampling(totalPacks, marksArray, normProb, offset):
-    mls = []
-    length = dict(nx.all_pairs_shortest_path_length(G))
-    for i in range(1,20):
-        G.nodes[i]['distance'] = length[20][i]
-        if marksArray[i]/totalPacks > (((1 - normProb) ** (G.nodes[i]['distance'])) * normProb) + offset:
-            mls.append(i)
-
-    return mls
-
-def edgeSampling(pathG ,edgeL, edgeDL, totalPacks, normProb, offset):
-
-    tupleList = []
-    suspiciousEdges = [0] * 21
-    mls = []
-    for i in range(0,len(edgeL)):
-    #print (i, ": ", G.nodes[20]['edgeList'][i])
-    #print (i, ": ", G.nodes[20]['edgeDList'][i])
-
-        #short = nx.shortest_path_length(G, edgeL[i][0], 20)
-        if (edgeDL[i] == 0):
-            pathG.add_edge(edgeL[i][0], 20, weight=0)
-            #print("Found weird distance from packet: ", i, " : ", G.nodes[20]['edgeList'][i], "Distance: ", G.nodes[20]['edgeDList'][i])
-
-        else:
-            pathG.add_edge(edgeL[i][0], edgeL[i][1], weight=edgeDL[i])
-            #insert edge (w.start,w.end,w.distance) into G
-
-    for u, v, a in pathG.edges(data=True):
-        #if pathG.has_edge(u, 20):
-
-        if pathG.has_edge(u, 20):
-            eShort = nx.shortest_path_length(pathG, u, 20)
-        
-        else:
-            eShort = 0
-
-        if eShort == a["weight"]:
-            #print("Found weird distance from packet: ", i, " : ", G.nodes[20]['edgeList'][i], "Distance: ", G.nodes[20]['edgeDList'][i])
-            #print(a, short)
-            #pathG.remove_edge(u, v)
-            tupleList.append((u,v))
-
-    for i in range(0,len(G.nodes[20]['edgeList'])):
-    #print (i, ": ", G.nodes[20]['edgeList'][i])
-    #print (i, ": ", G.nodes[20]['edgeDList'][i])
-
-        short = nx.shortest_path_length(G, G.nodes[20]['edgeList'][i][0], 20)
-        if (short < G.nodes[20]['edgeDList'][i]):
-            #print("Found weird distance from packet: ", i, " : ", G.nodes[20]['edgeList'][i], "Distance: ", G.nodes[20]['edgeDList'][i])
-            suspiciousEdges[G.nodes[20]['edgeList'][i][0]] += 1
-
-    for i in range(1,21):
-        if suspiciousEdges[i] > 20 * offset:
-            mls.append(i)
-
-    return mls
-
-
-
-###############################################################################################################################
-
-
-
-testRange = 3000
-
-print("Attacker1 was: ", attacker)
-print("Attacker2 was: ", attacker2)
-
-
-for i in range(0,testRange):
-        for i in range(1,21):
-            chance = rd.random()
-            if chance < G.nodes[i]['sendChance']:
-                totalPacketsSent += 1
-                sendPacket(G.nodes[i], G)
-
-nodeSuspects = nodeSampling(totalPacketsSent, G.nodes[20]['markCount'], G.nodes[20]['prob'], 0)
-print("Node Sampling Suspects: ", nodeSuspects)
-print("Total Packets Sent: ", totalPacketsSent)
-totalPacketsSent = 0
-
-for i in range(0,testRange):
-        for i in range(1,20):
-            chance = rd.random()
-            if chance < G.nodes[i]['sendChance']:
-                totalPacketsSent += 1
-                sendEdgePacket(G.nodes[i], G)
-
-edgeSuspects = edgeSampling(recPath, G.nodes[20]['edgeList'], G.nodes[20]['edgeDList'], totalPacketsSent, G.nodes[20]['prob'], 3)
-print("Edge Sampling Suspects: ", edgeSuspects)
-print("Total Packets Sent: ", totalPacketsSent)
-totalPacketsSent = 0
+# Close the output file
+sys.stdout.close()
